bb30757223f28ad7de0750bfeb6d3ee4523e9ef3
vadimivanov <Vjcrdf0508>
2017-01-17 12:55:20 +0200

    add MDT edit

--- src/app/rippleui/pages/generic-mdt/generic-mdt-detail.component.js
+++ src/app/rippleui/pages/generic-mdt/generic-mdt-detail.component.js
@@ -2,11 +2,35 @@ let templateGenericMdtDetail= require('./generic-mdt-detail.html');
 
 class GenericMdtDetailController {
   constructor($scope, $state, $stateParams, $ngRedux, genericmdtActions, GenericMdtModal, serviceRequests, usSpinnerService) {
+    $scope.isEdit = false;
+    
     this.edit = function () {
-      this.genericMdt.timeOfMeeting = new Date(this.genericMdt.timeOfMeeting);
-      GenericMdtModal.openModal(this.currentPatient, {title: 'Edit MDT'}, this.genericMdt, this.currentUser);
-    };
+      $scope.isEdit = true;
 
+      $scope.mdtEdit = Object.assign({}, this.genericMdt);
+      $scope.mdtEdit.dateSubmitted = new Date();
+    };
+    
+    this.cancelEdit = function () {
+      $scope.isEdit = false;
+    };
+    
+    $scope.confirmEdit = function (mdtForm, genericMdt) {
+      $scope.formSubmitted = true;
+      if (mdtForm.$valid) {
+        $scope.isEdit = false;
+        this.genericMdt = Object.assign(this.genericMdt, $scope.mdtEdit);
+        $scope.genericmdtUpdate(this.currentPatient.id, this.genericMdt);
+      }
+    }.bind(this);
+
+    $scope.openDatepicker = function ($event, name) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
+      $scope[name] = true;
+    };
+    
     this.setCurrentPageData = function (data) {
       if (data.patientsGet.data) {
         this.currentPatient = data.patientsGet.data;
@@ -28,6 +52,7 @@ class GenericMdtDetailController {
 
     this.genericmdtLoad = genericmdtActions.get;
     this.genericmdtLoad($stateParams.patientId, $stateParams.genericMdtIndex);
+    $scope.genericmdtUpdate = genericmdtActions.update;
   }
 }
 
