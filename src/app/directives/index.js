3e848bf69a93123fb30af25ee8da2e7a32127d43
vadimivanov <Vjcrdf0508>
2017-03-06 17:43:27 +0200

    fixed dicom

--- src/app/directives/index.js
+++ src/app/directives/index.js
@@ -137,8 +137,6 @@ angular.module('ripple-ui.directives', [])
           // Enable all tools we want to use with this element
           cornerstoneTools.wwwc.activate(element, 2); // ww/wc is the default tool for left mouse button
           cornerstoneTools.pan.activate(element, 1); // pan is the default tool for middle mouse button
-          cornerstoneTools.zoom.activate(element, 4); // zoom is the default tool for right mouse button
-          cornerstoneTools.zoomWheel.activate(element); // zoom is the default tool for middle mouse wheel
         });
       }
     };
