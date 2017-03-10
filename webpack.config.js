fa673a32c4cf0aefcb863bd8ce620dc2ff445787
vadimivanov <Vjcrdf0508>
2017-03-10 09:42:30 +0200

    add editable style

--- webpack.config.js
+++ webpack.config.js
@@ -65,9 +65,9 @@ const config = {
     new webpack.ProvidePlugin({
       angularSpinner: 'angular-spinner',
       _: 'underscore',
-      $: "jquery",
-      "window.jQuery": "jquery",
-      jQuery: "jquery",
+      // $: "jquery",
+      // "window.jQuery": "jquery",
+      // jQuery: "jquery",
       "moment": "moment",
       Raphael: 'raphael',
       io: 'socket.io-client'
